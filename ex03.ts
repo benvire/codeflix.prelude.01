8096727c989eb370b96b67791c98c3beb5a3caaa
Benjamin <benjamin.dupin@efrei.net>
2021-10-04 11:03:38 +0200

    adding ex03.ts

---
+++ ex03.ts
@@ -0,0 +1,15 @@
+let cdc:string = "teste"
+
+function Length(str = ""){
+    let result:number = 0
+
+
+    for(let i = 0; i >= 0; i++){
+        if(){}
+    }
+   
+}
+
+console.log(Length(cdc));
+console.log(cdc.length);
+
